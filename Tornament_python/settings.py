d6f6ad510b65b047df5c9846d807f0cacf6b3ae3
aditya <adityakothekar79@gmail.com>
2023-12-14 22:47:53 +0530

    te

--- Tornament_python/settings.py
+++ Tornament_python/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^a+!h88l41w*byml4-#=lplqtj5v1i%jji()z0slv(8g_@+l*-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh'] 
